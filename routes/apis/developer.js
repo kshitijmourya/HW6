b3e238dcc666702963d85f218cde6d6deef63e40
kshitijmourya <mourya.k@husky.neu.edu>
2018-08-20 04:59:07 -0400

    wrking

--- routes/apis/developer.js
+++ routes/apis/developer.js
@@ -4,6 +4,15 @@ var Developer = require('../../models/developer/developer.model.server');
 var express = require('express');
 var router = express.Router();
 
+
+router.post('/', (req, res) => {
+    const d = new Developer({username: req.body.username});
+    d.save((err, developer) => {
+        if (err) res.send(err);
+        else res.json(developer)
+    });
+});
+
 router.get('/', (req, res) => {
     Developer.find((err, developers) => {
         if (err) res.send(err);
@@ -19,35 +28,6 @@ router.get('/:did', (req, res) => {
         });
 });
 
-router.get('/:did/application', (req, res) => {
-    Developer.findById(req.params.did)
-        .populate('applications')
-        .exec()
-        .then(developer => res.json(developer.applications))
-        .catch(err => res.status(503).send(err))
-});
-
-
-router.post('/', (req, res) => {
-    const d = new Developer({username: req.body.username});
-    d.save((err, developer) => {
-        if (err) res.send(err);
-        else res.json(developer)
-    });
-});
-
-router.post('/:did/application', (req, res) => {
-    const application = new Application({name: req.body.name});
-    application.save()
-        .then(a => Developer.findByIdAndUpdate(
-            req.params.did,
-            {'$push': {applications: a.id}},
-            {'new': true})
-            .exec()
-            .then(() => res.json(a)))
-        .catch(err => res.status(503).send(err));
-});
-
 router.put('/:did', (req, res) => {
     Developer.findByIdAndUpdate(req.params.did, (err, developer) => {
         if (err) res.send(err);
@@ -61,11 +41,38 @@ router.put('/:did', (req, res) => {
     })
 });
 
+
 router.delete('/:did', (req, res) => {
     Developer.remove({_id: req.params.did}, (err, developer) => {
         if (err) res.send(err);
         else res.status(204).send();
     })
 });
+
+
+router.get('/:did/application', (req, res) => {
+    Developer.findById(req.params.did)
+        .populate('applications')
+        .exec()
+        .then(developer => res.json(developer.applications))
+        .catch(err => res.status(503).send(err))
+});
+
+
+
+
+router.post('/:did/application', (req, res) => {
+    const application = new Application({name: req.body.name});
+    application.save()
+        .then(a => Developer.findByIdAndUpdate(
+            req.params.did,
+            {'$push': {applications: a.id}},
+            {'new': true})
+            .exec()
+            .then(() => res.json(a)))
+        .catch(err => res.status(503).send(err));
+});
+
+
 
 module.exports = router;
